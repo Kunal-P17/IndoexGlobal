c9d600c300c91aa44c6f0cbf4c87f12a46e802d9
Kunal-P17 <144238648+Kunal-P17@users.noreply.github.com>
2026-07-08 02:11:30 +0530

    Frontend Changes 25

--- src/components/InquiryForm.tsx
+++ src/components/InquiryForm.tsx
@@ -133,8 +133,15 @@ export default function InquiryForm({ prefilledProductName, onClearPrefill }: In
 
             {/* Styled Google Maps iframe */}
             <div className="rounded-3xl overflow-hidden border border-slate-800 shadow-xl h-72">
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3784.080687498415!2d73.85814289999999!3d18.480003999999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2eb6b7f05ed99%3A0x2acc8296b2c9b04b!2sMahalaxmi%20Infra!5e0!3m2!1sen!2sin!4v1783456618788!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>
-            </div>
+<iframe
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3784.080687498415!2d73.85814289999999!3d18.480003999999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2eb6b7f05ed99%3A0x2acc8296b2c9b04b!2sMahalaxmi%20Infra!5e0!3m2!1sen!2sin!4v1783456618788!5m2!1sen!2sin"
+  width="100%"
+  height="100%"
+  style={{ border: 0 }}
+  allowFullScreen
+  loading="lazy"
+  referrerPolicy="strict-origin-when-cross-origin"
+/>            </div>
           </div>
 
           {/* Right Column: Inquiry Form Card */}
